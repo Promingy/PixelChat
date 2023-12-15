e4bbd1571feb75f7ad3e78012d3e3f57327de848
Nick Brooks <nicholasbrooks1@msn.com>
2023-12-15 15:57:38 -0800

    Update index.jsx

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -9,32 +9,12 @@ import Home from '../components/Home'
 import ServerPage from '../components/ServerPage'
 import ChannelPage from '../components/ChannelPage';
 
-// export const router = createBrowserRouter([
-//   {
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <h1>Welcome!</h1>,
-//       },
-//       {
-//         path: "login",
-//         element: <LoginFormPage />,
-//       },
-//       {
-//         path: "signup",
-//         element: <SignupFormPage />,
-//       },
-//     ],
-//   },
-// ]);
-
 export const router = createBrowserRouter(createRoutesFromElements(
   <Route element={<Layout />}>
 
-    <Route path='/'  element={<Home /> }/>
-    <Route path='/login' element={<LoginFormPage />}/>
-    <Route path='/signup' element={<SignupFormPage />}/>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<LoginFormPage />} />
+    <Route path='/signup' element={<SignupFormPage />} />
     <Route path='/landing' element={<LandingPage />} />
     <Route path='/main' element={<MainPage />}>
       <Route path='servers' element={<Outlet />}>
