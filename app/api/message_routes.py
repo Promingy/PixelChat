2134237be56c87c783ff6957da30beea352bc1c8
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-12-18 18:24:59 -0500

    create reactions bug

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, session
-from ..models import db, Message
+from ..models import db, Message, Reaction
 from flask_login import login_required
 
 message = Blueprint('message', __name__)
 
 @message.route('/<int:messageId>/reactions', methods=["POST"])
 @login_required
-def create_channel(messageId):
+def create_reactions(messageId):
+    message = Message.query.get(messageId)
+    # get reaction code from emojiPicker Modal
+    # csrf protect the data
+    # if res.ok:
+        # create new Reaction based on received code
+        # add new Reaction to db
+        # return new Reaction dictionary (to_dict)
+    # else:
+        # return errors dictionary w/ 401 status code
     pass
 
 @message.route('/<int:messageId>', methods=['DELETE'])
