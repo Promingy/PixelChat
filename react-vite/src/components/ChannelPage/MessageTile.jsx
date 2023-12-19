5cb5285317cf706389849471cde9e8ed93064991
Corbin <140107765+Promingy@users.noreply.github.com>
2023-12-18 18:38:36 -0700

    refactored the logic to be dryer, more organized and hover functionality
    known issue - when iterating through the object to create message tiles, order isn't preserved. need to sort by date in front end

---
+++ react-vite/src/components/ChannelPage/MessageTile.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import OpenModalButton from '../OpenModalButton/OpenModalButton'
+import EmojiPicker from 'emoji-picker-react'
+import './ChannelPage.css'
+
+
+export default function MessageTile({ message, user }) {
+    const [ reactBar, setReactBar ] = useState(false)
+
+    // format date
+    const date = new Date(message.created_at)
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    let day = date.getDate()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    const amPm = hours >= 12 ? 'PM' : 'AM'
+    hours = hours % 12 ? hours % 12 : 12
+    minutes = minutes < 10 ? `0${minutes}` : minutes
+
+    return (
+        <div className="user-message-container" onMouseOver={() => setReactBar(true)} onMouseLeave={() => setReactBar(false)}>
+
+            <div className="message-body-header-container">
+                <img className='message-profile-pic' src={user.image_url}/>
+
+                <div className="message-owner-date-container">
+
+                    <div className='date-name-container'>
+                        <span className="message-owner">{user.username}</span>
+                        <span className="message-post-time">{ month }-{ day }-{ year } { hours }:{ minutes } { amPm }</span>
+                    </div>
+                    <p className="message-body">{message.body}</p>
+                </div>
+            </div>
+
+           { reactBar && <div className={reactBar ? '' : 'hidden'}>
+                <OpenModalButton
+                    buttonText={'react'}
+                    modalComponent={<EmojiPicker />}
+                />
+            </div>}
+
+        </div>
+    )
+
+}
