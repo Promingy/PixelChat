92599e9ed8ded4a802cc3ea2d1dcec3c739bdab6
Corbin <140107765+Promingy@users.noreply.github.com>
2023-12-19 19:25:04 -0700

    fixed typo

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,7 +19,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_reactions
+        undo_reactions()
         undo_messages()
         undo_channels()
         undo_users()
