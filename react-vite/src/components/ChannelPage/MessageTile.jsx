1af942aad87c3717494a4708d329e29c671c122a
Nick Brooks <nicholasbrooks1@msn.com>
2023-12-20 13:04:52 -0800

    Added socket emit functionality to reaction CRUD

--- react-vite/src/components/ChannelPage/MessageTile.jsx
+++ react-vite/src/components/ChannelPage/MessageTile.jsx
@@ -6,9 +6,9 @@ import ReactionTile from './ReactionTile'
 import { useDispatch, useSelector } from 'react-redux'
 import { initializeReaction, removeReaction } from '../../redux/server'
 
-export default function MessageTile({ message, user, channelId }) {
+export default function MessageTile({ message, user, channelId, socket }) {
     const dispatch = useDispatch()
-    const [ reactBar, setReactBar ] = useState(false)
+    const [reactBar, setReactBar] = useState(false)
     const sessionUser = useSelector(state => state.session.user)
 
     // format date
@@ -21,52 +21,52 @@ export default function MessageTile({ message, user, channelId }) {
 
     // set object with a count of each emoji
     const reactions = {}
-   // iterate over every reaction and add them to the reactions counter / increment
-    for (let reaction of Object.values(message.reactions)){
+    // iterate over every reaction and add them to the reactions counter / increment
+    for (let reaction of Object.values(message.reactions)) {
         reactions[reaction.emoji] = reactions[reaction.emoji] ? reactions[reaction.emoji] + 1 : 1
     }
 
     return (
         <>
-        <div className="user-message-container" onMouseOver={() => setReactBar(true)} onMouseLeave={() => setReactBar(false)}>
+            <div className="user-message-container" onMouseOver={() => setReactBar(true)} onMouseLeave={() => setReactBar(false)}>
 
-            <div className="message-body-header-container">
-                <img className='message-profile-pic' src={user.image_url}/>
+                <div className="message-body-header-container">
+                    <img className='message-profile-pic' src={user.image_url} />
 
-                <div className="message-owner-date-container">
+                    <div className="message-owner-date-container">
 
-                    <div className='date-name-container'>
-                        <span className="message-owner">{user.username}</span>
-                        <span className="message-post-time">{ hours }:{ minutes } { amPm }</span>
+                        <div className='date-name-container'>
+                            <span className="message-owner">{user.username}</span>
+                            <span className="message-post-time">{hours}:{minutes} {amPm}</span>
+                        </div>
+                        <p className="message-body">{message.body}</p>
                     </div>
-                    <p className="message-body">{message.body}</p>
+
                 </div>
 
+                {reactBar && <div className={reactBar ? '' : 'hidden'}>
+                    <OpenModalButton
+                        buttonText={'react'}
+                        modalComponent={<EmojiPicker
+                            //if an emoji is selected through the picker, add it to the database!
+                            onEmojiClick={(e) => {
+                                //remove the reaction if user has already used it
+                                for (let reaction of Object.values(message.reactions)) {
+                                    if (reaction.user_id == sessionUser.id && reaction.emoji == e.emoji) {
+                                        return dispatch(removeReaction(channelId, message.id, reaction.id))
+                                    }
+                                }
+                                // if user hasn't used this reaction already, add reaction
+                                return dispatch(initializeReaction(channelId, { user_id: sessionUser.id, message_id: message.id, emoji: e.emoji }))
+                            }}
+                        />}
+                    />
+                </div>}
             </div>
 
-           { reactBar && <div className={reactBar ? '' : 'hidden'}>
-                <OpenModalButton
-                    buttonText={'react'}
-                    modalComponent={<EmojiPicker
-                    //if an emoji is selected through the picker, add it to the database!
-                    onEmojiClick={(e) => {
-                        //remove the reaction if user has already used it
-                        for (let reaction of Object.values(message.reactions)){
-                            if (reaction.user_id == sessionUser.id && reaction.emoji == e.emoji) {
-                                return dispatch(removeReaction(channelId, message.id, reaction.id))
-                            }
-                        }
-                        // if user hasn't used this reaction already, add reaction
-                        return dispatch(initializeReaction(channelId, {user_id: sessionUser.id, message_id: message.id, emoji: e.emoji}))
-                        }}
-                    />}
-                />
-            </div>}
-        </div>
-
-        <div className='message-reactions-container'>
+            <div className='message-reactions-container'>
                 {Object.keys(reactions).map(key => {
-                    return <ReactionTile key={key} allReactions={message.reactions} channelId={channelId} reaction={key} count={reactions[key]} messageId={message.id} />
+                    return <ReactionTile key={key} allReactions={message.reactions} channelId={channelId} reaction={key} socket={socket} count={reactions[key]} messageId={message.id} />
                 })}
             </div>
         </>
