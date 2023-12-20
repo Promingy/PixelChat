5ca970f7a39bb6924fb83e76c4c56df8d59dcd34
Esther <zhangyun024@gmail.com>
2023-12-20 14:58:59 -0800

    channel creation done

---
+++ react-vite/src/components/ChannelCreationForm/ChannelCreationForm.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useModal } from "../../context/Modal";
+import { initializeChannel } from "../../redux/server";
+import { VscChromeClose } from "react-icons/vsc";
+import "./ChannelCreationForm.css";
+
+export default function ChannelCreationForm() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { closeModal } = useModal();
+
+  const serverId = useSelector((state) => state.server.id);
+  const [name, setName] = useState("");
+  const [topic, setTopic] = useState("");
+  const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleChannelCreation = async (e) => {
+    e.preventDefault();
+
+    const channel = {
+      name,
+      topic,
+      description,
+    };
+
+    const channelData = await dispatch(initializeChannel(serverId, channel));
+    return navigate(`main/servers/${serverId}/channels/${channelData.id}`);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Create a channel</h1>
+        <button type="button" onClick={closeModal}>
+          <VscChromeClose />
+        </button>
+      </div>
+      <form onSubmit={handleChannelCreation} className="channel-creation-form">
+        <label>
+          Name*
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="e.g.plan-budget"
+            required
+          />
+        </label>
+        {errors.name && <p>{errors.name}</p>}
+        <label>
+          Description
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        {errors.description && <p>{errors.description}</p>}
+
+        <label>
+          Topic
+          <input
+            type="text"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </label>
+        {errors.topic && <p>{errors.topic}</p>}
+        <button type="submit" className="create-button">
+          Create
+        </button>
+      </form>
+    </>
+  );
+}
--- react-vite/src/redux/server.js
+++ react-vite/src/redux/server.js
@@ -103,7 +103,7 @@ export const loadServer = (serverId) => async (dispatch) => {
 }
 
 export const removeServer = (serverId) => async (dispatch) => {
-    const res = await fetch(`api/servers/${serverId}`, {
+    const res = await fetch(`/api/servers/${serverId}`, {
         method: "DELETE"
     })
     if (res.ok) {
@@ -115,7 +115,7 @@ export const removeServer = (serverId) => async (dispatch) => {
 }
 
 export const editServer = (server, serverId) => async (dispatch) => {
-    const res = await fetch(`api/servers/${serverId}`, {
+    const res = await fetch(`/api/servers/${serverId}`, {
         method: "PUT",
         body: JSON.stringify(server),
         headers: {
@@ -133,7 +133,7 @@ export const editServer = (server, serverId) => async (dispatch) => {
 }
 
 export const initializeServer = (server) => async (dispatch) => {
-    const res = await fetch(`api/servers`, {
+    const res = await fetch(`/api/servers`, {
         method: "POST",
         body: JSON.stringify(server),
         headers: {
@@ -176,7 +176,7 @@ export const editChannel = (channel, channelId) => async (dispatch) => {
 }
 
 export const initializeChannel = (serverId, channel) => async (dispatch) => {
-    const res = await fetch(`api/servers/${serverId}/channels`, {
+    const res = await fetch(`/api/servers/${serverId}/channels`, {
         method: "POST",
         body: JSON.stringify(channel),
         headers: {
@@ -191,7 +191,7 @@ export const initializeChannel = (serverId, channel) => async (dispatch) => {
 }
 
 export const removeMessage = (channelId, messageId) => async (dispatch) => {
-    const res = await fetch(`api/messages/${messageId}`, {
+    const res = await fetch(`/api/messages/${messageId}`, {
         method: "DELETE"
     })
     if (res.ok) {
@@ -201,7 +201,7 @@ export const removeMessage = (channelId, messageId) => async (dispatch) => {
 }
 
 export const initializeMessage = (channelId, message) => async (dispatch) => {
-    const res = await fetch(`api/channels/${channelId}/messages`, {
+    const res = await fetch(`/api/channels/${channelId}/messages`, {
         method: "POST",
         body: JSON.stringify(message),
         headers: {
@@ -295,7 +295,6 @@ const serverReducer = (state = initialState, action) => {
         }
         case UPDATE_CHANNEL: {
             const newState = { ...state }
-            console.log(action.channel)
             newState.channels[action.channel.id] = { ...action.channel }
             return newState
         }
