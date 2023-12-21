cf8145b07f04fb9956cc3c71a7f8bd89db87c9d4
Corbin <140107765+Promingy@users.noreply.github.com>
2023-12-20 17:34:49 -0700

    Implemented partial functionality for infinite scroll

--- react-vite/src/components/ChannelPage/ChannelPage.jsx
+++ react-vite/src/components/ChannelPage/ChannelPage.jsx
@@ -1,21 +1,23 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './ChannelPage.css'
 import MessageTile from "./MessageTile";
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import ChannelPopupModal from "../ChannelPopupModal/ChannelPopupModal";
 import MessageBox from '../MessageBox'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { getMessages } from "../../redux/server";
 
 
 export default function ChannelPage({ socket }) {
+    const dispatch = useDispatch()
     const { channelId } = useParams()
     const server = useSelector(state => state.server)
     const channel = server?.channels?.[+channelId]
     const messages = server?.channels?.[+channelId]?.messages
     const users = server?.users
-    const [message, setMessage] = useState();
-
+    const [ offset, setOffset ] = useState(15)
 
     function generate_message_layout() {
         // func to iterate over all messages for a channel
@@ -71,23 +73,24 @@ export default function ChannelPage({ socket }) {
                 {users && <OpenModalButton
                     buttonText={`${Object.keys(users).length} Members`}
                     modalComponent={<ChannelPopupModal activeProp={2} />}
-                />}
+                    />}
 
-            <div className="all-messages-container">
+            <div className="all-messages-container" id='all-messages-container'>
                     {generate_message_layout()}
+                {messages && <InfiniteScroll
+                    dataLength={Object.values(messages).length}
+                    hasMore={!(Object.values(messages).length % 15)}
+                    next={() => {
+                        dispatch(getMessages(channelId, `offset=${offset}`))
+                        setOffset(prevOffset => prevOffset += 15)
+                    }}
+                    inverse={true}
+                    scrollableTarget='all-messages-container'
+
+                    />}
                 </div>
                 <MessageBox socket={socket} serverId={server.id}channelName={channel?.name} channelId={channelId}/>
             </div>
-
-            {/* <OpenModalButton
-                    buttonText={channel?.name}
-                    modalComponent={<ChannelPopupModal activeProp={1} />}
-                /> */}
-
-            {/* {users && <OpenModalButton
-                    buttonText={`${Object.keys(users).length} Members`}
-                    modalComponent={<ChannelPopupModal activeProp={2} />}
-                />} */}
         </>
     )
 }
