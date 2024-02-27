bad156b30fee6d35c4d091c930d4e6a9aed3b05c
Esther <zhangyun024@gmail.com>
2024-02-27 01:18:06 -0800

    toolbar init

--- react-vite/src/components/ChannelPage/MessageTile.jsx
+++ react-vite/src/components/ChannelPage/MessageTile.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { initializeReaction, removeReaction, removeMessage } from '../../redux/server'
 import { thunkPinMessage } from '../../redux/server'
 import ProfileModal from "../ProfileModal";
-import EmojiPicker from 'emoji-picker-react'
+import EmojiPicker from 'emoji-picker-react';
+import parse from "html-react-parser";
 
 
 export default function MessageTile({ message, user, channelId, socket }) {
@@ -18,6 +19,10 @@ export default function MessageTile({ message, user, channelId, socket }) {
     const [profileModal2, setProfileModal2] = useState(false)
     const [emojiBox, setEmojiBox] = useState(false)
     const [emojiBoxHeight, setEmojiBoxHeight] = useState(0)
+    const replaceClass = function (htmlString) {
+        const updatedHtml = htmlString.replace(/class=/g, "className=");
+        return updatedHtml;
+    };
 
     // format date
     const date = new Date(message.created_at)
@@ -87,156 +92,208 @@ export default function MessageTile({ message, user, channelId, socket }) {
     }
 
     return (
-        <>
-            {profileModal &&
-                <div className='profile-modal-messages'>
-                    <ProfileModal animation={false} userId={message.user_id} />
+      <>
+        {profileModal && (
+          <div className="profile-modal-messages">
+            <ProfileModal animation={false} userId={message.user_id} />
+          </div>
+        )}
+        {profileModal2 && (
+          <div className="profile-modal-messages">
+            <ProfileModal animation={true} userId={message.user_id} />
+          </div>
+        )}
+        {emojiBox && (
+          <div
+            className={"emoji-box"}
+            id="emojiBox"
+            style={{ top: `${emojiBoxHeight}px` }}
+          >
+            <EmojiPicker
+              theme={localStorage.getItem("theme") || "light"}
+              // if an emoji is selected through the picker, add it to the database!
+              onEmojiClick={(e) => {
+                // remove the reaction if user has already used it
+                setEmojiBox(false);
+                for (let reaction of Object.values(message.reactions)) {
+                  if (
+                    reaction.user_id == sessionUser.id &&
+                    reaction.emoji == e.emoji
+                  ) {
+                    return dispatch(
+                      removeReaction(channelId, message.id, reaction.id)
+                    ).then(() => {
+                      const payload = {
+                        type: "reaction",
+                        method: "DELETE",
+                        room: +server?.id,
+                        channelId,
+                        messageId: message.id,
+                        reactionId: reaction.id,
+                      };
+
+                      socket.emit("server", payload);
+                    });
+                  }
+                }
+                // if user hasn't used this reaction already, add reaction
+                return dispatch(
+                  initializeReaction(channelId, message.id, { emoji: e.emoji })
+                ).then((res) => {
+                  const payload = {
+                    type: "reaction",
+                    method: "POST",
+                    room: +server?.id,
+                    channelId,
+                    reaction: res,
+                  };
+                  socket.emit("server", payload);
+                });
+              }}
+            />
+          </div>
+        )}
+
+        <div
+          className={
+            message.pinned
+              ? "user-message-container pinned "
+              : `user-message-container`
+          }
+          onMouseOver={() => setReactBar(true)}
+          onMouseLeave={() => {
+            setReactBar(false);
+            setConfirmMsgDel(false);
+          }}
+        >
+          <div className="message-body-reactions-container">
+            {message.pinned && (
+              <i className="fa-sharp fa-solid fa-thumbtack fa-xs pin-message pinned-icon-on-message" />
+            )}
+            <div className="message-body-header-container">
+              <img
+                className="message-profile-pic"
+                src={user?.image_url}
+                id={message.id}
+                onClick={() => {
+                  setProfileModal(true);
+
+                  window.addEventListener("mousedown", handleProfileModal);
+                }}
+              />
+
+              <div className="message-owner-date-container">
+                <div
+                  className="date-name-container"
+                  onClick={() => {
+                    setProfileModal(true);
+
+                    window.addEventListener("mousedown", handleProfileModal);
+                  }}
+                >
+                  <p className="message-owner" id={message.id}>
+                    {user?.username}
+                  </p>
+                  <p className="message-post-time" id={message.id}>
+                    {hours}:{minutes}
+                  </p>
+                  <p className="message-post-time" id={message.id}>
+                    {amPm}
+                  </p>
                 </div>
-            }
-            {profileModal2 &&
-                <div className='profile-modal-messages'>
-                    <ProfileModal animation={true} userId={message.user_id} />
-                </div>
-            }
-            {emojiBox &&
-                <div className={'emoji-box'} id="emojiBox" style={{ 'top': `${emojiBoxHeight}px` }}>
-                    <EmojiPicker
-                        theme={localStorage.getItem('theme') || 'light'}
-
-                        // if an emoji is selected through the picker, add it to the database!
-                        onEmojiClick={(e) => {
-                            // remove the reaction if user has already used it
-                            setEmojiBox(false)
-                            for (let reaction of Object.values(message.reactions)) {
-                                if (reaction.user_id == sessionUser.id && reaction.emoji == e.emoji) {
-                                    return dispatch(removeReaction(channelId, message.id, reaction.id)).then(() => {
-                                        const payload = {
-                                            type: 'reaction',
-                                            method: 'DELETE',
-                                            room: +server?.id,
-                                            channelId,
-                                            messageId: message.id,
-                                            reactionId: reaction.id
-                                        }
-
-                                        socket.emit("server", payload)
-                                    })
-                                }
-                            }
-                            // if user hasn't used this reaction already, add reaction
-                            return dispatch(initializeReaction(channelId, message.id, { emoji: e.emoji })).then(res => {
-                                const payload = {
-                                    type: 'reaction',
-                                    method: 'POST',
-                                    room: +server?.id,
-                                    channelId,
-                                    reaction: res
-                                }
-                                socket.emit("server", payload)
-                            })
-                        }} />
-                </div>}
-
-            <div className={message.pinned ? 'user-message-container pinned ' : `user-message-container`} onMouseOver={() => setReactBar(true)} onMouseLeave={() => {
-                setReactBar(false)
-                setConfirmMsgDel(false)
-            }}>
-
-                <div className='message-body-reactions-container'>
-                    {message.pinned &&
-                        <i className='fa-sharp fa-solid fa-thumbtack fa-xs pin-message pinned-icon-on-message' />
-                    }
-                    <div className="message-body-header-container">
-                        <img
-                            className='message-profile-pic'
-                            src={user?.image_url}
-                            id={message.id}
-                            onClick={() => {
-                                setProfileModal(true)
-
-                                window.addEventListener('mousedown', handleProfileModal)
-                            }}
-                        />
-
-                        <div className="message-owner-date-container">
-                            <div
-                                className='date-name-container'
-                                onClick={() => {
-                                    setProfileModal(true)
-
-                                    window.addEventListener('mousedown', handleProfileModal)
-                                }}>
-                                <p className="message-owner" id={message.id}>{user?.username}</p>
-                                <p className="message-post-time" id={message.id}>{hours}:{minutes}</p>
-                                <p className="message-post-time" id={message.id}>{amPm}</p>
-                            </div>
-                            <p className="message-body">{message.body}</p>
-                        </div>
-
-                    </div>
-
-                    <div className='message-reactions-container'>
-                        {Object.keys(reactions).map(key => {
-                            return <ReactionTile key={key} socket={socket} serverId={server?.id} allReactions={message.reactions} channelId={channelId} reaction={key} count={reactions[key]} messageId={message.id} />
-                        })}
-                    </div>
-
-                </div >
-
-                {<div className={`react-bar${reactBar ? "" : ' react-bar-hidden'} ${localStorage.getItem('theme') === 'dark' ? 'react-bar-dark' : ''}`}>
-
-                    <i className='fa-solid fa-face-laugh-wink fa-lg reaction-icon'
-                        onClick={(e) => handleEmojiBox(e)} />
-
-                    {server.owner_id === sessionUser.id &&
-                        <i
-                            className='fa-sharp fa-solid fa-thumbtack pin-message'
-                            onClick={() => {
-                                const updatedMessage = { ...message }
-                                updatedMessage.pinned = !updatedMessage.pinned
-                                dispatch(thunkPinMessage(updatedMessage)).then(res => {
-                                    const payload = {
-                                        type: 'message',
-                                        method: 'PUT',
-                                        room: +server?.id,
-                                        channelId,
-                                        message: res
-                                    }
-                                    socket.emit("server", payload)
-                                })
-                            }} />
-                    }
-
-
-                    {sessionUser.id === message.user_id && <div>
-                        <i className='fa-regular fa-trash-can remove-message' onClick={() => setConfirmMsgDel(!confirmMsgDel)} />
-                        {confirmMsgDel && <div
-                            className='confirm-message-delete'
-                            onMouseLeave={() => setConfirmMsgDel(false)}
-                            onClick={() => {
-                                setConfirmMsgDel(false)
-                                dispatch(removeMessage(channelId, message.id)).then(() => {
-                                    const payload = {
-                                        userId: sessionUser.id,
-                                        type: 'message',
-                                        method: 'DELETE',
-                                        room: +server?.id,
-                                        channelId,
-                                        messageId: message.id
-                                    }
-
-
-                                    socket.emit("server", payload)
-                                })
-                            }}
-                        >Confirm Delete</div>}
-                    </div>}
-                </div>}
-
+                <p className="message-body">
+                  {parse(replaceClass(message.body))}
+                  {/* {message.body} */}
+                </p>
+              </div>
             </div>
 
-        </>
+            <div className="message-reactions-container">
+              {Object.keys(reactions).map((key) => {
+                return (
+                  <ReactionTile
+                    key={key}
+                    socket={socket}
+                    serverId={server?.id}
+                    allReactions={message.reactions}
+                    channelId={channelId}
+                    reaction={key}
+                    count={reactions[key]}
+                    messageId={message.id}
+                  />
+                );
+              })}
+            </div>
+          </div>
 
-    )
+          {
+            <div
+              className={`react-bar${reactBar ? "" : " react-bar-hidden"} ${
+                localStorage.getItem("theme") === "dark" ? "react-bar-dark" : ""
+              }`}
+            >
+              <i
+                className="fa-solid fa-face-laugh-wink fa-lg reaction-icon"
+                onClick={(e) => handleEmojiBox(e)}
+              />
+
+              {server.owner_id === sessionUser.id && (
+                <i
+                  className="fa-sharp fa-solid fa-thumbtack pin-message"
+                  onClick={() => {
+                    const updatedMessage = { ...message };
+                    updatedMessage.pinned = !updatedMessage.pinned;
+                    dispatch(thunkPinMessage(updatedMessage)).then((res) => {
+                      const payload = {
+                        type: "message",
+                        method: "PUT",
+                        room: +server?.id,
+                        channelId,
+                        message: res,
+                      };
+                      socket.emit("server", payload);
+                    });
+                  }}
+                />
+              )}
+
+              {sessionUser.id === message.user_id && (
+                <div>
+                  <i
+                    className="fa-regular fa-trash-can remove-message"
+                    onClick={() => setConfirmMsgDel(!confirmMsgDel)}
+                  />
+                  {confirmMsgDel && (
+                    <div
+                      className="confirm-message-delete"
+                      onMouseLeave={() => setConfirmMsgDel(false)}
+                      onClick={() => {
+                        setConfirmMsgDel(false);
+                        dispatch(removeMessage(channelId, message.id)).then(
+                          () => {
+                            const payload = {
+                              userId: sessionUser.id,
+                              type: "message",
+                              method: "DELETE",
+                              room: +server?.id,
+                              channelId,
+                              messageId: message.id,
+                            };
+
+                            socket.emit("server", payload);
+                          }
+                        );
+                      }}
+                    >
+                      Confirm Delete
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          }
+        </div>
+      </>
+    );
 
 }
--- react-vite/src/components/MessageBox/MessageBox.jsx
+++ react-vite/src/components/MessageBox/MessageBox.jsx
@@ -3,10 +3,57 @@ import './MessageBox.css'
 import { useState } from "react";
 import { initializeMessage } from '../../redux/server'
 import { useDispatch } from 'react-redux'
+import React, { useMemo, useRef } from "react";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
 
 export default function MessageBox({ socket, channelName, channelId, serverId }) {
     const dispatch = useDispatch()
     const [message, setMessage] = useState('')
+    // const renderCustomToolBar = () =>{
+    //     return(
+    //     <>
+    //         <span className='ql-formats'>
+    //             <button className="ql-bold" aria-label='Bold'> </button>
+    //             <button className="ql-italic" aria-label='Italic'> </button>
+    //             <button className="ql-strike" aria-label='Strike'> </button>
+    //         </span>
+    //         <span className='ql-formats'>
+    //             <button className="ql-link" aria-label='Link'> </button>
+    //         </span>
+    //         <span className='ql-formats'>
+    //             <button className="ql-link" aria-label='Link'> </button>
+    //         </span>
+    //     </>
+    //     )
+    // }
+
+    // const header = renderCustomToolBar()
+ 
+    const removeTags = function (str) {
+      if (str === null || str === "") return false;
+      else str = str.toString();
+      return str.replace(/(<([^>]+)>)/gi, "");
+    };
+
+    // console.log("===", message);
+    // console.log("~~~", message.length);
+    // console.log("~~~", removeTags(message));
+    // console.log("~~~", removeTags(message).length);
+
+    const modules = useMemo(
+        () => ({
+          toolbar: {
+            container: [
+              ["bold", "italic", "strike"],
+              ["link"],
+              [{ list: "ordered" }, { list: "bullet" }],
+              ["code-block"],
+            ],
+          },
+        }),
+        []
+      );
 
     document.documentElement.className = `theme-${localStorage.getItem('theme') || 'light'}`;
 
@@ -44,29 +91,75 @@ export default function MessageBox({ socket, channelName, channelId, serverId })
     }
 
     return (
-        <form className='send-message-form'>
-            <div className='message-wrapper-top'>
-                <TextareaAutoSize
-                    className="message-box"
-                    value={message}
-                    onChange={e => setMessage(e.target.value)}
-                    placeholder={`Message #${channelName}`}
-                    onKeyUp={(e) => {
-                        if (e.key === 'Enter' &&
-                            !!message.match(/[A-Za-z0-9!@?#$&()\\-`.+,/\\]/g) &&
-                            message.length <= 2001) {
-                            return handleSubmit(e)
-                        }
-                    }} />
+      <>
+        <div className="send-message-form">
+          <ReactQuill
+            theme="snow"
+            onChange={(value) => setMessage(value)}
+            modules={modules}
+            value={message}
+            className="message-box"
+            placeholder={`Message #${channelName}`}
+            style={{ border: "none" }}
+              onKeyUp={(e) => {
+                if (
+                  e.key === "Enter" &&
+                  !!message.match(/[A-Za-z0-9!@?#$&()\\-`.+,/\\]/g) &&
+                  message.length <= 2001
+                ) {
+                  return handleSubmit(e);
+                }
+              }}
+          />
+          {/* <Editor
+          onTextChange={(e) => setMessage(e.htmlValue)}
+         headerTemplate={header}
+          value={message}
+          className="message-box"
+          placeholder={`Message #${channelName}`}
+        /> */}
 
+          {/* <div className="message-wrapper-top">
+            <TextareaAutoSize
+              className="message-box"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder={`Message #${channelName}`}
+              onKeyUp={(e) => {
+                if (
+                  e.key === "Enter" &&
+                  !!message.match(/[A-Za-z0-9!@?#$&()\\-`.+,/\\]/g) &&
+                  message.length <= 2001
+                ) {
+                  return handleSubmit(e);
+                }
+              }}
+            />
+          </div> */}
+          <div className="message-wrapper-bottom">
+            <div className="char-count-and-submit">
+              <span
+                className={
+                  removeTags(message).length >= 1800
+                    ? removeTags(message) >= 2000
+                      ? "over-message-limit"
+                      : "nearing-message-limit"
+                    : `clear-message-limit`
+                }
+              >
+                {removeTags(message).length}/2000
+              </span>
+              <button
+                disabled={
+                  !message.match(/[A-Za-z0-9!@?#$&()\\-`.+,/\\]/g) ||
+                  removeTags(message).length > 2000
+                }
+                onClick={handleSubmit}
+                className={`fa-regular fa-paper-plane fa-lg send-message`}
+              />
             </div>
-            <div className='message-wrapper-bottom'>
-                <div className='char-count-and-submit'>
-                    <span className={message.length >= 1800 ? message.length >= 2000 ? 'over-message-limit' : 'nearing-message-limit' : `clear-message-limit`}>{message.length}/2000</span>
-                    <button disabled={!message.match(/[A-Za-z0-9!@?#$&()\\-`.+,/\\]/g) || message.length > 2000} onClick={handleSubmit} className={`fa-regular fa-paper-plane fa-lg send-message`} />
-                </div>
-
-            </div>
-        </form >
-    )
+          </div>
+        </div>
+      </>
+    );
 }
